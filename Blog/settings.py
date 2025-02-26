cfea8b9c508d6b352ec8dfd2568ac0036c225e6f
Abhijeet Gupta <abhijeetg40@gmail.com>
2025-02-26 19:17:31 +0530

    Update settings.py

--- Blog/settings.py
+++ Blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-it&g9oad!ao_f=1fhyl^+0#&(lcb+%s8@y#lbs!5%t+5dludr_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
